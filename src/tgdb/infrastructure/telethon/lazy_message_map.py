b4d34bd96dae5184482e7ea3b96afce9fb0f058a
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-06-03 21:16:02 +0700

    fix: fix linter errors

--- src/tgdb/infrastructure/telethon/lazy_message_map.py
+++ src/tgdb/infrastructure/telethon/lazy_message_map.py
@@ -5,7 +5,7 @@ from telethon.tl.types import Message
 
 from tgdb.entities.relation.tuple import TID
 from tgdb.infrastructure.heap_tuple_encoding import HeapTupleEncoding
-from tgdb.infrastructure.lazy_map import LazyMap
+from tgdb.infrastructure.lazy_map import ExternalValue, LazyMap, NoExternalValue
 from tgdb.infrastructure.telethon.client_pool import TelegramClientPool
 
 
@@ -15,7 +15,7 @@ type LazyMessageMap = LazyMap[TID, Message]
 def lazy_message_map(
     chat_id: int, pool: TelegramClientPool, computed_map_max_len: int
 ) -> LazyMessageMap:
-    async def tuple_message(tid: TID) -> Message | None:
+    async def tuple_message(tid: TID) -> ExternalValue[Message]:
         search = HeapTupleEncoding.id_of_encoded_tuple_with_tid(tid)
 
         messages = cast(TotalList, await pool().get_messages(
@@ -23,8 +23,8 @@ def lazy_message_map(
         ))
 
         if not messages:
-            return None
+            return NoExternalValue()
 
-        return cast(Message | None, messages[0])
+        return cast(Message, messages[0])
 
     return LazyMap(computed_map_max_len, tuple_message)
