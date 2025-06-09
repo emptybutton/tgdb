65f8bd2438d55cdd60522e37532bc8b877d09e66
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-06-09 21:09:04 +0700

    fix(`test_config`): use new config path

--- tests/test_tgdb/test_infrastructure/test_pyyaml/test_config.py
+++ tests/test_tgdb/test_infrastructure/test_pyyaml/test_config.py
@@ -4,4 +4,4 @@ from tgdb.infrastructure.pyyaml.config import TgdbConfig
 
 
 def test_no_errors() -> None:
-    TgdbConfig.load(Path("deploy/dev/tgdb/conf.yaml"))
+    TgdbConfig.load(Path("deploy/common/tgdb/config/config.yaml"))
